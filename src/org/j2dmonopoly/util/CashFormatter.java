b0486e4428874d6b1a3e74f40579b9c753639c5d
93111117 <salvationsentinel@yahoo.com>
2015-07-05 03:47:46 +0430

    Initial commit

---
+++ src/org/j2dmonopoly/util/CashFormatter.java
@@ -0,0 +1,15 @@
+//Created by zavaraki & mirzaei
+
+package org.j2dmonopoly.util;
+
+import java.text.DecimalFormat;
+
+public class CashFormatter
+{
+	private static DecimalFormat formatter = new DecimalFormat("##,###");
+
+	public static String format(final int startingAmount)
+	{
+		return formatter.format(startingAmount);
+	}
+}
